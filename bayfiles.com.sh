b7ec06a78736587d82fd3abb923673d801894ca7
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2022-03-23 23:47:18 +0100

    bayfiles.com

---
+++ bayfiles.com.sh
@@ -0,0 +1,11 @@
+#/bin/bash/
+#bayfiles.com
+echo 'Enter THE FULL PATH of the file'
+read path
+url=$(curl -F "file=@$path" https://api.bayfiles.com/upload | jq -r '.data.file.url.short')
+filename=$(printf '%b' "${path//%/\\x}")
+myfilesize=$(wc -c "$path" | awk '{print $1}')
+echo "filename : $filename"
+echo "size in byte : $myfilesize"
+echo "Short Link : $url"
+echo "Long Link: $url/$filename"
