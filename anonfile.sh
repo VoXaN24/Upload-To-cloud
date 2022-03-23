d21611aabe9f4b6ebd8a8120f0dd2fa90899a7a1
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2022-03-23 23:33:41 +0100

    Create anonfile.sh

---
+++ anonfile.sh
@@ -0,0 +1,10 @@
+#/bin/bash/
+echo 'Enter THE FULL PATH of the file'
+read path
+url=$(curl -F "file=@$path" https://api.anonfiles.com/upload | jq -r '.data.file.url.short')
+filename=$(printf '%b' "${path//%/\\x}")
+myfilesize=$(wc -c "$path" | awk '{print $1}')
+echo "filename : $filename"
+echo "size in byte : $myfilesize"
+echo "Short Link : $url"
+echo "Long Link: $url/$filename"
